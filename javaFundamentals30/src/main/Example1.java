3b3280495870d5d25dfdadd19354d9f9e23154eb
VladCraciunescu <svcraciunescu@yahoo.com>
2020-11-30 18:56:36 +0200

    first

--- javaFundamentals30/src/main/Example1.java
+++ javaFundamentals30/src/main/Example1.java
@@ -1,4 +1,13 @@
 package main;
 
+import java.util.Scanner;
+
 public class Example1 {
+    public static void main(String[] args) {
+
+
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        System.out.println(x);
+    }
 }
